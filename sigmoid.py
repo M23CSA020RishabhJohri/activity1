2f6805fb27a004251e99b806d946e69eb2ad9ee6
M23CSA020RishabhJohri <m23csa020@iitj.ac.in>
2024-03-27 02:19:41 +0530

    Add sigmoid function and print results

---
+++ sigmoid.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+random_values = np.array([-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6])
+print("Sigmoid Results:", sigmoid(random_values))
